270cf4219cb553a368807e6dbb99542e0130085c
nerfall <nerfall10@gmail.com>
2025-01-31 04:09:40 +0200

    update final main page

--- js/script.js
+++ js/script.js
@@ -10,59 +10,149 @@ const languageText = languageButton?.querySelector("p");
 let selectedLanguage = "ua";
 
 if (languageButton && languageList) {
-    languageButton.addEventListener("click", (event) => {
-        event.stopPropagation();
-        languageList.classList.toggle("open");
-        document[languageList.classList.contains("open") ? "addEventListener" : "removeEventListener"]("click", handleOutsideClick);
-    });
+  languageButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    languageList.classList.toggle("open");
+    document[
+      languageList.classList.contains("open")
+        ? "addEventListener"
+        : "removeEventListener"
+    ]("click", handleOutsideClick);
+  });
 
-    languageList.addEventListener("click", (event) => {
-        const button = event.target.closest("button");
-        if (button) {
-            selectedLanguage = button.dataset.lang;
-            if (languageText) languageText.textContent = selectedLanguage.toUpperCase();
-            updateSelectedLanguage();
-            languageList.classList.remove("open");
-            document.removeEventListener("click", handleOutsideClick);
-        }
-    });
-
-    function handleOutsideClick(event) {
-        if (!languageList.contains(event.target) && !languageButton.contains(event.target)) {
-            languageList.classList.remove("open");
-            document.removeEventListener("click", handleOutsideClick);
-        }
+  languageList.addEventListener("click", (event) => {
+    const button = event.target.closest("button");
+    if (button) {
+      selectedLanguage = button.dataset.lang;
+      if (languageText)
+        languageText.textContent = selectedLanguage.toUpperCase();
+      updateSelectedLanguage();
+      languageList.classList.remove("open");
+      document.removeEventListener("click", handleOutsideClick);
     }
+  });
 
-    function updateSelectedLanguage() {
-        languageList.querySelectorAll("button").forEach((button) => {
-            button.classList.toggle("selected", button.dataset.lang === selectedLanguage);
-        });
+  function handleOutsideClick(event) {
+    if (
+      !languageList.contains(event.target) &&
+      !languageButton.contains(event.target)
+    ) {
+      languageList.classList.remove("open");
+      document.removeEventListener("click", handleOutsideClick);
     }
+  }
 
-    updateSelectedLanguage();
+  function updateSelectedLanguage() {
+    languageList.querySelectorAll("button").forEach((button) => {
+      button.classList.toggle(
+        "selected",
+        button.dataset.lang === selectedLanguage
+      );
+    });
+  }
+
+  updateSelectedLanguage();
 }
 
 // Modal
-const modal = document.getElementById("modal");
-  const closeModal = document.getElementById("closeModal");
-  const openModalButtons = document.querySelectorAll(".btn_white, .btn_blue, #contactButton");
+const modalMain = document.getElementById("modal_main1");
+const modalSuccess = document.getElementById("modal_main2");
+const closeModal = document.getElementById("closeModal");
+const form = document.getElementById("form_main_big");
+const submitButton = document.getElementById("form_main_submit");
+const successButton = document.getElementById("contact_button");
+const openModalButtons = document.querySelectorAll(".form_main");
 
-  openModalButtons.forEach(button => {
-      button.addEventListener("click", () => {
-          if (modal) {
-              modal.style.display = "flex";
-          }
-      });
-  });
+const closeModalFunction = (modal) => {
+    if (modal) {
+        modal.classList.remove("active");
+        setTimeout(() => {
+            modal.style.display = "none";
+        }, 350);
+    }
+};
 
-  closeModal?.addEventListener("click", () => {
-      if (modal) {
-          modal.style.display = "none";
-      }
+openModalButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        e.stopPropagation();
+        modalMain.style.display = "flex";
+        requestAnimationFrame(() => modalMain.classList.add("active"));
+    });
+});
+
+closeModal.addEventListener("click", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    closeModalFunction(modalMain);
+});
+
+successButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    closeModalFunction(modalSuccess);
+});
+
+document.addEventListener("click", (e) => {
+    if (modalMain.classList.contains("active") && !form.contains(e.target)) {
+        closeModalFunction(modalMain);
+    }
+    if (modalSuccess.classList.contains("active") && !document.getElementById("succes_container").contains(e.target)) {
+        closeModalFunction(modalSuccess);
+    }
+});
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    closeModalFunction(modalMain);
+    setTimeout(() => {
+        modalSuccess.style.display = "flex";
+        requestAnimationFrame(() => modalSuccess.classList.add("active"));
+    }, 400);
+});
+// select project
+document.querySelectorAll(".select_work").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    button.classList.toggle("checked");
   });
+});
+//price
+document.querySelectorAll(".select_price").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    document
+      .querySelectorAll(".select_price")
+      .forEach((btn) => btn.classList.remove("checked"));
+    button.classList.add("checked");
+  });
+});
+//selector
+document.querySelector(".select-box").addEventListener("click", function () {
+  const dropdown = this.closest(".select-wrapper");
+  dropdown.classList.toggle("active");
+});
+
+document.querySelectorAll(".dropdown-item").forEach((item) => {
+  item.addEventListener("click", function () {
+    const selectBox =
+      this.closest(".select-wrapper").querySelector(".selected");
+    selectBox.textContent = this.textContent;
+    selectBox.dataset.value = this.dataset.value;
+    this.closest(".select-wrapper").classList.remove("active");
+  });
+});
+
+document.addEventListener("click", function (e) {
+  const isDropdown = e.target.closest(".select-wrapper");
+  if (!isDropdown) {
+    document.querySelectorAll(".select-wrapper.active").forEach((dropdown) => {
+      dropdown.classList.remove("active");
+    });
+  }
+});
 // Header Scroll Behavior
-const style = document.createElement('style');
+const style = document.createElement("style");
 style.textContent = `
       .header {
         position: fixed;
@@ -80,18 +170,18 @@ document.head.appendChild(style);
 
 let lastScrollPosition = 0;
 let ticking = false;
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 
 if (header) {
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     const currentScrollPosition = window.pageYOffset;
 
     if (!ticking) {
       window.requestAnimationFrame(() => {
         if (currentScrollPosition > lastScrollPosition) {
-          header.classList.add('hidden');
+          header.classList.add("hidden");
         } else {
-          header.classList.remove('hidden');
+          header.classList.remove("hidden");
         }
         lastScrollPosition = currentScrollPosition;
         ticking = false;
